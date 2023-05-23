cb5f15a6b7acf11dadcd9e87847d3ea086599e8c
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-05-23 08:13:08 -0300

    Create dia23maio.py

---
+++ dia23maio.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Teste ECMI')
+
+st.write("Tabela")
+
+dataframe = pd.DataFrame({
+     'Nome': ['Josir', 'Bruno', 'Bruna', 'Anna'], 
+     'Salário': [10, 20, 30, 40]
+})
+dataframe.style.highlight_max(axis=0)
+
+st.write(dataframe)
