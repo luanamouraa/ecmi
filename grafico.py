bbbcb038c7fe253df6fcfd4d2fd753b879eb2a2c
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-05-30 08:46:12 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -7,3 +7,14 @@ chart_data = pd.DataFrame(
     columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data) 
+
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
