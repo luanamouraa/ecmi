0a86596baf05f33603a7f30be570e5ac61783da9
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-05-30 08:47:33 -0300

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -18,3 +18,13 @@ chart_data = pd.DataFrame(
     columns=["a", "b", "c"])
 
 st.bar_chart(chart_data)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c'])
+
+st.area_chart(chart_data)
