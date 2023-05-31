a688cdbdac10fa48be2d5071cfd0c55924542e5c
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-31 12:04:20 -0300

    Update tarefacsv.py

--- tarefacsv.py
+++ tarefacsv.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
+
 st.title('Trabalho csv')
 st.caption('Luana Moura')
 
-df = pd.read_csv('starbucks.csv', sep=' , ')
+df = pd.read_csv('starbucks.csv')
 st.dataframe(df)
