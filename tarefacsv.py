dfd917ca8a29027a760280d2d0ccce38c1dfc558
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-31 12:06:40 -0300

    Update tarefacsv.py

--- tarefacsv.py
+++ tarefacsv.py
@@ -4,5 +4,5 @@ import pandas as pd
 st.title('Trabalho csv')
 st.caption('Luana Moura')
 
-df = pd.read_csv('starbucks.csv')
+df = pd.read_csv('starbucks.csv', sep=',', quotechar='"')
 st.dataframe(df)
