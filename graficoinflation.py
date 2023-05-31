f25f60d15f37044084252b6d0d048fca810fc0d2
luanamouraa <133766513+luanamouraa@users.noreply.github.com>
2023-05-31 11:44:39 -0300

    Update and rename graficosalaries.py to graficoinflation.py

--- graficoinflation.py
+++ graficoinflation.py
@@ -3,11 +3,11 @@ import pandas as pd
 st.title('Arquivo csv')
 st.caption('Luana Moura')
 
-df = pd.read_csv('ds_salaries.csv', sep=',')
+df = pd.read_csv('ADO April 2023 - Inflation.csv', sep=',')
 st.dataframe(df)
 
-import numpy as np
 
+import numpy as np
 chart_data = pd.DataFrame(
     np.random.randn(20, 3),
     columns=['a', 'b', 'c'])
